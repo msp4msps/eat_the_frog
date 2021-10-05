7a21984a6aa8b480193daae68e754feb6e045b77
jbarker <jackson.barker@du.edu>
2021-10-04 21:50:48 -0600

    joke-boc hide function and button

--- script.js
+++ script.js
@@ -4,9 +4,13 @@ var currentTime = moment().format("MM/DD/YYYY");
 
 time.textContent = currentTime;
 
+// joke variables
 var btn = document.querySelector(".btn");
 var jokeTxt = document.querySelector(".joke-container");
+var hideBtn = document.querySelector(".hide-btn");
+var jokeBox = document.querySelector(".joke-box")
 
+// displays joke on load
 document.addEventListener("DOMContentLoaded", getJoke);
 
 btn.addEventListener("click", getJoke);
@@ -19,4 +23,13 @@ function getJoke() {
   })
     .then((data) => data.json())
     .then((object) => (jokeTxt.innerHTML = object.joke));
+}
+
+//hideBtn hides joke-box
+hideBtn.addEventListener("click", function() {
+  hideJoke();
+})
+
+function hideJoke() {
+  jokeBox.style.display='none';
 }
