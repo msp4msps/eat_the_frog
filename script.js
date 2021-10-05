a2e33df54c07e24f4e7f2bc7a309422b90e8eb4a
jonjackson <87287862+jonteal@users.noreply.github.com>
2021-10-04 21:28:27 -0500

    Initial commit of weather app. App is working

---
+++ script.js
@@ -0,0 +1,45 @@
+var searchBtn = document.getElementById("search-btn");
+var city = document.getElementById("search-bar");
+
+let weather = {
+    apiKey: "275bd71fcc87ee1fd19695e4aee1f3bb",
+    fetchWeather: function(city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q="
+            + city 
+            + "&units=imperial&appid="
+            + this.apiKey
+        )
+            .then((response) => response.json())
+            .then((data) => {
+                this.displayWeather(data);
+            })
+    },
+
+
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp } = data.main;
+
+        document.getElementById("city").innerText = name;
+        document.getElementById("icon").src = "http://openweathermap.org/img/wn/" + icon + ".png";
+        document.getElementById("description").innerText = description;
+        document.getElementById("temp").innerText = temp + "° F";
+    },
+    search: function() {
+        this.fetchWeather(document.getElementById("search-bar").value);
+    }
+};
+
+// Event Listener for Search Button upon Click
+document.getElementById("search-btn").addEventListener("click", function() {
+    weather.search();
+})
+
+// Event Listener for Search Bar if user hits Enter Key
+document.getElementById("search-bar").addEventListener("keyup", function(event) {
+    if (event.key == "Enter") {
+        weather.search();
+    }
+})
