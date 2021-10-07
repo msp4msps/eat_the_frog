7786d83f750d9deb2c9fa22ed05190473ef62e7a
Jesse Draper <jessedraper2@gmail.com>
2021-10-06 18:19:12 -0600

    preclass commit

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 var SaveMe = document.getElementById("saveBtn");
 var inProgList = document.getElementById("inProg");
 SaveMe.addEventListener("click", addFrog);
-
-function addFrog() {
-  var infoZone = document.getElementById("MyTextArea").innerText;
+//callback for saveme event listener//
+function addFrog(event) {
+  event.preventDefault();
+  var infoZone = document.getElementById("MyTextArea").value;
   localStorage.setItem("frog", infoZone);
   var newLine = document.createElement("li");
-  newLine.textContent(infoZone);
-  inProgList.append(newLine);
+  newLine.innerText(infoZone);
   var checkbox = document.createElement("input");
   checkbox.type = "checkbox";
-  checkbox.value = 1;
-  checkbox.name = "todo[]";
+  console.log(infoZone);
+  console.log(newLine);
+  inProgList.appendChild(newLine + checkbox);
+  
+
+  for (let i = 0; i < 4; i++) {
+      while (inProgList.length < 4){
+
+      }};
 }
