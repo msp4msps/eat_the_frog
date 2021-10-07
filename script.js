99717008e1b078a2825855d04591d38a5e801ae3
msp4msps <msp4msps@tminus365.com>
2021-10-06 18:55:26 -0600

    Updates to task

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
-
 var SaveMe = document.getElementById("saveBtn");
 var inProgList = document.getElementById("inProg");
-SaveMe.addEventListener("click", addFrog);
 
-function addFrog() {
-  var infoZone = document.getElementById("MyTextArea").innerText;
+function addFrog(event) {
+  event.preventDefault();
+  var infoZone = document.getElementById("MyTextArea").value;
   localStorage.setItem("frog", infoZone);
   var newLine = document.createElement("li");
-  newLine.textContent(infoZone);
+  newLine.textContent = infoZone;
   inProgList.append(newLine);
   var checkbox = document.createElement("input");
   checkbox.type = "checkbox";
   checkbox.value = 1;
   checkbox.name = "todo[]";
-  
+}
+
+SaveMe.addEventListener("click", addFrog);
+
 var time = document.getElementById("time");
 
 var currentTime = moment().format("MM/DD/YYYY");
